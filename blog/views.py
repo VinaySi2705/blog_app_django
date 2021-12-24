e47fd00f4315b6c3424198ada15a12d6dd299b1a
Vinay Singh Yadav <vinaysinghyadav9125@gmail.com>
2021-12-24 19:16:38 +0530

    last commit

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,57 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.contrib.auth.mixins import (
+            LoginRequiredMixin,
+            UserPassesTestMixin)
+from django.views.generic import (
+        ListView,
+        DetailView,
+        CreateView,
+        UpdateView,
+        DeleteView) #during creating class based view it's form is alreday been made we have to just provide the fields
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView): #this use the same template post_form.html on it's own
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
